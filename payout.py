e78a9e07b9fb51d554c44e171d36d09fa5c1dfbb
Erik Gustafsson <Erikegse@gmail.com>
2024-01-07 19:22:14 +0100

    Add files via upload

--- payout.py
+++ payout.py
@@ -16,7 +16,7 @@ from datetime import datetime
 
 testing = "NO"
 
-hive = Hive(node="https://anyx.io", nobroadcast=False, expiration = 60, num_retries=3)
+hive = Hive(node="https://hived.emre.sh", nobroadcast=False, expiration = 60, num_retries=3)
 he_node = "https://api.primersion.com/"
 he_api = Api(url=he_node)
 
